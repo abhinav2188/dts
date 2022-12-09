b6ca6ab05d32a90d449e28726a8e19d056d78534
abhinav2188 <abhinav.mca19.du@gmail.com>
2022-12-09 13:08:00 +0530

    updated excel cells data types

--- src/main/java/com/example/art/services/helper/ExcelHelper.java
+++ src/main/java/com/example/art/services/helper/ExcelHelper.java
@@ -1,18 +1,14 @@
 package com.example.art.services.helper;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import com.example.art.model.views.DealExcelView;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Component;
 
@@ -20,9 +16,12 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
+@Slf4j
 public class ExcelHelper {
 
     public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+
+    public static CellStyle DATE_CELL_STYLE;
 
     public void listToExcel(List<DealExcelView> objects, Class<?> clazz, String sheetName, HttpServletResponse response) {
 
@@ -68,6 +67,9 @@ public class ExcelHelper {
 
     public <T>void listToSheet(Workbook workbook, String sheetName, List<T> objects) {
 
+        // Date cell style
+        CellStyle dateCellStyle = createDateCellStyle(workbook);
+
         if(objects.size() == 0) return;
         Class<?> clazz = objects.get(0).getClass();
 
@@ -89,20 +91,41 @@ public class ExcelHelper {
             // data rows
             int rowIdx = 1;
             for(Object object : objects){
+                log.debug("data object: {}",object);
                 Row row = sheet.createRow(rowIdx++);
                 col = 0;
                 for(Field field : fields){
                     Cell cell = row.createCell(col++);
                     field.setAccessible(true);
-                    cell.setCellValue(String.valueOf(field.get(object)));
+                    Object val = field.get(object);
+                    if(val == null){
+                        // do nothing
+                    }else if(val instanceof Date){
+                        cell.setCellStyle(dateCellStyle);
+                        cell.setCellValue((Date) val);
+                    }else if(val instanceof Number){
+                        cell.setCellValue(Double.parseDouble(String.valueOf(val)));
+                    }else if(val instanceof Boolean){
+                        cell.setCellValue((boolean)val);
+                    }else{
+                        cell.setCellValue(String.valueOf(val));
+                    }
                     field.setAccessible(false);
                 }
+                log.debug("row created: {}", row);
             }
 
         } catch (IllegalAccessException e) {
             e.printStackTrace();
             throw new RuntimeException("error while reading values from object using reflect: "+e.getMessage());
         }
+    }
+
+    private CellStyle createDateCellStyle(Workbook workbook) {
+        CreationHelper createHelper =workbook.getCreationHelper();
+        CellStyle dateCellStyle = workbook.createCellStyle();
+        dateCellStyle.setDataFormat(createHelper.createDataFormat().getFormat("dd/mm/yy hh:mm"));
+        return dateCellStyle;
     }
 
 }
